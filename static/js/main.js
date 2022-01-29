6a8f48c33695df8ea49acacb24a2eef6d4e7a5c0
GargSamarth <samarthgarg0901@gmail.com>
2022-01-29 15:37:21 +0530

    changes praposed

--- static/js/main.js
+++ static/js/main.js
@@ -147,49 +147,49 @@
 
     //speakers carousel
 
-    window.onscroll = function () {
-        //var info5Y = document.getElementById('info5').offsetTop;
-        var info4Y = document.getElementById('info4').offsetTop;
-        var info3Y = document.getElementById('info3').offsetTop;
-        var info2Y = document.getElementById('info2').offsetTop;
-        var info1Y = document.getElementById('info1').offsetTop;
-        var currentScrollPos = window.pageYOffset;
-        if(currentScrollPos + 80 < info4Y){
-            document.getElementById('ed5').style.opacity = "1"
-            document.getElementById('ed4').style.opacity = "0.7";
-            document.getElementById('ed3').style.opacity = "0.7";
-            document.getElementById('ed2').style.opacity = "0.7";
-            document.getElementById('ed1').style.opacity = "0.7";
-        }  
-        else if(currentScrollPos + 80 >= info4Y && currentScrollPos + 80 < info3Y){
-            document.getElementById('ed5').style.opacity = "0.7"
-            document.getElementById('ed4').style.opacity = "1";
-            document.getElementById('ed3').style.opacity = "0.7";
-            document.getElementById('ed2').style.opacity = "0.7";
-            document.getElementById('ed1').style.opacity = "0.7";
-        }  
-        else if(currentScrollPos + 80 >= info3Y && currentScrollPos + 80 < info2Y){
-            document.getElementById('ed5').style.opacity = "0.7"
-            document.getElementById('ed4').style.opacity = "0.7";
-            document.getElementById('ed3').style.opacity = "1";
-            document.getElementById('ed2').style.opacity = "0.7";
-            document.getElementById('ed1').style.opacity = "0.7";
-        }
-        else if(currentScrollPos +80 >= info2Y && currentScrollPos + 80 < info1Y){
-            document.getElementById('ed5').style.opacity = "0.7"
-            document.getElementById('ed4').style.opacity = "0.7";
-            document.getElementById('ed3').style.opacity = "0.7";
-            document.getElementById('ed2').style.opacity = "1";
-            document.getElementById('ed1').style.opacity = "0.7";
-        }   
-        else{
-            document.getElementById('ed5').style.opacity = "0.7"
-            document.getElementById('ed4').style.opacity = "0.7";
-            document.getElementById('ed3').style.opacity = "0.7";
-            document.getElementById('ed2').style.opacity = "0.7";
-            document.getElementById('ed1').style.opacity = "1";
-        }
-    }
+    // window.onscroll = function () {
+    //     var info5Y = document.getElementById('info5').offsetTop;
+    //     var info4Y = document.getElementById('info4').offsetTop;
+    //     var info3Y = document.getElementById('info3').offsetTop;
+    //     var info2Y = document.getElementById('info2').offsetTop;
+    //     var info1Y = document.getElementById('info1').offsetTop;
+    //     var currentScrollPos = window.pageYOffset;
+    //     if(currentScrollPos + 80 < info4Y){
+    //         document.getElementById('ed5').style.opacity = "1"
+    //         document.getElementById('ed4').style.opacity = "0.7";
+    //         document.getElementById('ed3').style.opacity = "0.7";
+    //         document.getElementById('ed2').style.opacity = "0.7";
+    //         document.getElementById('ed1').style.opacity = "0.7";
+    //     }  
+    //     else if(currentScrollPos + 80 >= info4Y && currentScrollPos + 80 < info3Y){
+    //         document.getElementById('ed5').style.opacity = "0.7"
+    //         document.getElementById('ed4').style.opacity = "1";
+    //         document.getElementById('ed3').style.opacity = "0.7";
+    //         document.getElementById('ed2').style.opacity = "0.7";
+    //         document.getElementById('ed1').style.opacity = "0.7";
+    //     }  
+    //     else if(currentScrollPos + 80 >= info3Y && currentScrollPos + 80 < info2Y){
+    //         document.getElementById('ed5').style.opacity = "0.7"
+    //         document.getElementById('ed4').style.opacity = "0.7";
+    //         document.getElementById('ed3').style.opacity = "1";
+    //         document.getElementById('ed2').style.opacity = "0.7";
+    //         document.getElementById('ed1').style.opacity = "0.7";
+    //     }
+    //     else if(currentScrollPos +80 >= info2Y && currentScrollPos + 80 < info1Y){
+    //         document.getElementById('ed5').style.opacity = "0.7"
+    //         document.getElementById('ed4').style.opacity = "0.7";
+    //         document.getElementById('ed3').style.opacity = "0.7";
+    //         document.getElementById('ed2').style.opacity = "1";
+    //         document.getElementById('ed1').style.opacity = "0.7";
+    //     }   
+    //     else{
+    //         document.getElementById('ed5').style.opacity = "0.7"
+    //         document.getElementById('ed4').style.opacity = "0.7";
+    //         document.getElementById('ed3').style.opacity = "0.7";
+    //         document.getElementById('ed2').style.opacity = "0.7";
+    //         document.getElementById('ed1').style.opacity = "1";
+    //     }
+    // }
     
 
     //sponsors carousel
